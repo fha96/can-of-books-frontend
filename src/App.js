c90b1362de5ddf17c96346456c4c6b399449194e
fha96 <fahadalrimawe96@hotmail.com>
2022-08-08 15:53:59 +0300

    add auth0 feature

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-
+import Profile from './components/Profile';
 class App extends React.Component {
+ 
   render() {
     return (
       <>
         <Router>
           <Header />
+
           <Routes>
             <Route 
               exact path="/"
               element={<BestBooks />}
             >
+              </Route>
+            <Route 
+              exact path="/profile"
+              element={<Profile/>}
+            >
             </Route>
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
           </Routes>
+          
           <Footer />
         </Router>
       </>
