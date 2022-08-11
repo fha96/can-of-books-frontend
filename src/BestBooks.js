204f7d5eae5ccec6eb00f1af39695c46e1be5865
fha96 <fahadalrimawe96@hotmail.com>
2022-08-11 23:07:15 +0300

    complete another features

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,6 +3,7 @@ import { withAuth0 } from '@auth0/auth0-react';
 import Welcome from './components/Welcome';
 import axios from 'axios';
 import BooksTable from './components/BooksTable';
+import Form from './components/Form';
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
@@ -16,12 +17,34 @@ class BestBooks extends React.Component {
   
   // http://localhost:3001/books
   componentDidMount= async()=>{
-    const url=`${process.env.REACT_APP_HEROKU}/books`;
+    const url=`${process.env.REACT_APP_EXPRESS}/books`;
     let booksdata= await axios.get(url);
     // console.log(booksdata.data);
     this.setState({
       books:booksdata.data
     });
+  }
+  deleteBook= async(id)=>{
+    console.log(id);
+    const url=`${process.env.REACT_APP_EXPRESS}/books/${id}`;
+    await axios.delete(url);
+    this.componentDidMount();
+  }
+
+  createBook= async(e)=>{
+    e.preventDefault();
+    
+    console.log(e.target[0].value);
+    const newBook={
+      title:e.target[0].value,
+      decription : e.target[1].value,
+      status : e.target[2].value
+    }
+    const url=`${process.env.REACT_APP_EXPRESS}/books`;
+    await axios.post(url,{newBook});
+
+    this.componentDidMount();
+
   }
   render() {
 
@@ -33,13 +56,14 @@ class BestBooks extends React.Component {
       <div className='bookBest'>
 
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+        <Form submitHandler={this.createBook}/>
         {this.state.books.length ? (
           this.state.books.map(item=>{
             return(
           <div key={item._id}>
-            <BooksTable title={item.title} description={item.decription} status={item.status}/>
-          {/* <p >{item.title} : </p> */}
+            <BooksTable title={item.title} description={item.decription} status={item.status} />
+              <button onClick={()=>this.deleteBook(item._id)}>Delete</button>
+              
           </div>
           )
           })
